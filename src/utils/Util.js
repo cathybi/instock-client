4c32109ca6cc67dcf93700c9945bfc26ccb84bc5
Tim Liu <timho1985@gmail.com>
2024-03-13 13:19:53 -0700

    J24BTW-20: Front-End: Inventory List Component (Responsive)
    Finished based Inventory List Component

---
+++ src/utils/Util.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export class InStockApi {
+    constructor() {
+        this.baseUrl = "http://localhost:5050";
+        this.axios = axios.create({
+            baseURL: this.baseUrl,
+        });
+    }
+
+    async getInventoryList() {
+        try {
+            const response = await this.axios.get("/api/inventories");
+            return response.data;
+        } catch (error) {
+            throw new Error("Failed to get inventory list: " + error.message);
+        }
+    }
+}
