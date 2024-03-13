759f801b6e8283ab0d08a95f83fa3c61d7328cbf
noshinbashar <noshinbashar027@gmail.com>
2024-03-12 17:50:29 -0700

    Updated header and footer jsx files

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,37 @@
-import "./Header.scss";
+import './Header.scss';
+import logo from '../../assets/logos/InStock-Logo_2x.png';
+import { Link, NavLink } from 'react-router-dom';
 
 /**
  * 
+ * 
+ * 
  * @returns 
  */
-const Header = () => {
-    return(
-        <p>Header</p>
-    );
+
+function Header() {
+    return (
+        <>
+            <div className="header-background"></div>
+
+            <nav className="nav">
+
+                <Link to="/warehouse" className="nav__logo-link" >
+                    <img src={logo} className="nav__logo" alt="logo" />
+                </ Link>
+
+                <div className="nav__page-link">
+                    <NavLink ClassName="nav__link" to="/warehouse" >
+                        <h3 className="nav__link-title">Warehouses</h3>
+                    </ NavLink>
+                    <NavLink ClassName="nav__link" to="/inventory" >
+                        <h3 className="nav__link-title">Inventory</h3>
+                    </ NavLink>
+                </div>
+
+            </nav>
+        </>
+    )
 }
 
-export default Header;
+export default Header
