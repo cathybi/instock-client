690f765c25d4db65e704c3d2022fd9a8f531a3bd
noshinbashar <noshinbashar027@gmail.com>
2024-03-14 14:59:24 -0700

    created style for mobile

--- src/components/AddEditInventory/AddEditInventory.jsx
+++ src/components/AddEditInventory/AddEditInventory.jsx
@@ -24,7 +24,8 @@ function AddEditInventory() {
                     <Link className="inventory-item__link" to="/inventory">
                         <img className="inventory-item__icon-arrow" src={arrow} alt="Back Arrow" />
                     </Link>
-                    <h1 className="inventory-item__heading">{inventoryItem.itemName}</h1>
+                    {/* <h1 className="inventory-item__heading">{inventoryItem.itemName}</h1> */}
+                    <h1 className="inventory-item__heading">Television</h1>
                     <Link className="inventory-item__link-edit" to={`/inventory/${inventoryItem.id}/edit`} >
                         <img className="inventory-item__icon-edit" src={edit} alt="Edit Item" />
                         <p className="inventory-item__icon-text">Edit</p>
@@ -38,30 +39,37 @@ function AddEditInventory() {
 
                     <div className="inventory-item__details">
                         <h4 className="inventory-item__subheading">ITEM DESCRIPTION:</h4>
-                        <p className="inventory-item__information">{inventoryItem.description}</p>
+                        {/* <p className="inventory-item__information">{inventoryItem.description}</p> */}
+                        <p className="inventory-item__information">This 50", 4K LED TV provides a crystal-clear picture and vivid colors.</p>
                     </div>
 
                     <div className="inventory-item__details">
                         <h4 className="inventory-item__subheading">CATEGORY:</h4>
-                        <p className="inventory-item__information">{inventoryItem.category}</p>
+                        {/* <p className="inventory-item__information">{inventoryItem.category}</p> */}
+                        <p className="inventory-item__information">Electronics</p>
                     </div>
 
                     <div className="inventory-item__details-status">
                         <h4 className="inventory-item__subheading">STATUS:</h4>
-                        <p className={inventoryItem.status === "In Stock" ? "inventory-item__information--green" : 'inventory-item__information--red'} > 
+                        {/* <p className={inventoryItem.status === "In Stock" ? "inventory-item__information--green" : "inventory-item__information--red"} > 
                             {inventoryItem.status}
+                        </p> */}
+                        <p className={inventoryItem.status === "In Stock" ? "inventory-item__information--green" : "inventory-item__information--red"} > 
+                            {"In Stock"}
                         </p>
                         
                     </div>
 
                     <div className="inventory-item__details-quantity">
                         <h4 className="inventory-item__subheading">QUANTITY:</h4>
-                        <p className="inventory-item__information">{inventoryItem.quantity}</p>
+                        {/* <p className="inventory-item__information">{inventoryItem.quantity}</p> */}
+                        <p className="inventory-item__information">500</p>
                     </div>
 
                     <div className="inventory-item__details">
                         <h4 className="inventory-item__subheading">WAREHOUSE:</h4>
-                        <p className="inventory-item__information">{inventoryItem.warehouseName}</p>
+                        {/* <p className="inventory-item__information">{inventoryItem.warehouseName}</p> */}
+                        <p className="inventory-item__information">Manhattan</p>
                     </div>
 
                 </div>
