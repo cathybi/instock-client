2611aee38364a98db1dfb5fa8b015a0b0a1644ef
Trupti <patiltruptib@gmail.com>
2024-03-14 12:27:34 -0700

    Fixed console error

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     <Routes>
 
       <Route path="/inventory" element={< InventoryPage/>} />
-      <Route exact path="/" element={< WarehousesPage/>} />
+      <Route path="/" element={< WarehousesPage/>} />
       <Route path="/inventory/add" element={< AddEditInventory/>} />
       <Route path="/warehouse/add" element={< AddEditWarehouse/>} />
       <Route path="/inventory/:inventoryId" element={< SelectedInventory/>} />
