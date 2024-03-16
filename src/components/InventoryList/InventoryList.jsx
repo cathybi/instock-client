3a247255e0ade7758c177a01f57e80cecb9e8396
Tim Liu <timho1985@gmail.com>
2024-03-16 01:45:51 -0700

    J24BTW-11: Front-End: Warehouse Inventory List
    - Refactory inventory list component to be able to reuse in both WarehousesDetailPage and InventoryList page
    - Add J24BTW-33 animations to arrow icon to move 4px away when hover in inventory page.
    - Improve styling in WarehousesDetailPage
    - Query database to get WarehouseDetailPage data

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -1,39 +1,19 @@
 import "./InventoryList.scss"
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef } from 'react';
 import { Link } from "react-router-dom";
-import { InStockApi } from "../../utils/Util";
 import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/icons/edit-24px.svg";
 import sortIcon from "../../assets/icons/sort-24px.svg";
 import rightIcon from "../../assets/icons/chevron_right-24px.svg";
 
-function InventoryList() {
-    const [inventoryList, setInventoryList] = useState("");
-    const [filter, setFilter] = useState("");
+function InventoryList({ inventoryList, displayWarehouse }) {
     const searchRef = useRef();
-    const api = new InStockApi();
-
-    const initInventoryList = async () => {
-        const inventoryList = await api.getInventoryList();
-        setInventoryList(inventoryList);
-    };
-
-    useEffect(() => {
-        document.title = 'Inventory';
-        searchRef.current.value = "";
-        initInventoryList();
-    }, []);
 
     function handleSearch(event) {
         event.preventDefault();
         if (event.key === 'Enter' || event.keyCode === 13) {
             let searchQuery = searchRef.current.value.trim();
             console.log("Search string is: ", searchQuery);
-            if (!searchQuery) {
-                setFilter(null);
-            } else {
-                setFilter(searchQuery);
-            }
         }
     }
 
@@ -52,100 +32,105 @@ function InventoryList() {
     }
 
     return (
-        <div className="inventory">
-            <form className="inventory__form">
-                <h1 className="inventory__header">Inventory</h1>
-                <div className="inventory__header-right">
-                    <input className="inventory__input" type="text" id="search" name="search" onKeyUp={handleSearch} placeholder="Search..." ref={searchRef} />
-                    <button className="inventory__button" onClick={handleAddItem}>+ Add New Item</button>
-                </div>
-            </form>
-            <div>
-                <ul className="inventory__list">
-                    <li key="0" className="inventory__tablet-li">
-                        <div className="inventory__tablet-sectionwraper">
-                            <div className="inventory__tablet-section--left">
-                                <div className="inventory__tablet-column">
-                                    <h4 className="inventory__tablet-header">INVENTORY ITEM</h4>
-                                    <img className="inventory__tablet-icon" alt="sort icon" src={sortIcon} />
-                                </div>
-                                <div className="inventory__tablet-column">
-                                    <h4 className="inventory__tablet-header">CATEGORY</h4>
-                                    <img className="inventory__tablet-icon" alt="sort icon" src={sortIcon} />
-                                </div>
-                            </div>
-                            <div className="inventory__tablet-section--right">
-                                <div className="inventory__tablet-column">
-                                    <h4 className="inventory__tablet-header inventory__tablet-header--status">STATUS</h4>
-                                    <img className="inventory__tablet-icon" alt="sort icon" src={sortIcon} />
-                                </div>
-                                <div className="inventory__tablet-column">
-                                    <h4 className="inventory__tablet-header inventory__tablet-header--qty">QTY</h4>
-                                    <img className="inventory__tablet-icon" alt="sort icon" src={sortIcon} />
-                                </div>
-                                <div className="inventory__tablet-column">
-                                    <h4 className="inventory__tablet-header">WAREHOUSE</h4>
-                                    <img className="inventory__tablet-icon" alt="sort icon" src={sortIcon} />
-                                </div>
-                            </div>
+        <div className={displayWarehouse ? "inventory overlap" : "inventory overlap-disable"}>
+            {
+                displayWarehouse && (
+                    <form className="inventory__form">
+                        <h1 className="inventory__header">Inventory</h1>
+                        <div key="999" className="inventory__header-right">
+                            <input className="inventory__input" type="text" id="search" name="search" onKeyUp={handleSearch} placeholder="Search..." ref={searchRef} />
+                            <button className="inventory__button" onClick={handleAddItem}>+ Add New Item</button>
                         </div>
-                        <div className="inventory__tablet-action">
-                            <h4 className="inventory__tablet-header">ACTIONS</h4>
-                        </div>
-                    </li>
-                    {
-                        inventoryList &&
-                        inventoryList
-                            .filter((item) => !filter || item.item_name.toLowerCase().includes(filter.toLowerCase()))
-                            .map((inventory) => (
-                                <>
-                                    <hr className="inventory__line" />
-                                    <li key={inventory.id} className="inventory__li">
-                                        <div className="inventory__sectionwraper">
-                                            <div className="inventory__section--left">
-                                                <div className="inventory__column">
-                                                    <h4 className="inventory__item-header">INVENTORY ITEM</h4>
-                                                    <div className="inventory__item-container">
-                                                        <Link className="inventory-item__link" to={`/inventory/${inventory.id}`}>
-                                                            <div className="inventory__item--blue">{inventory.item_name}</div>
-                                                            <img className="inventory__item-icon" alt="right icon" src={rightIcon} />
-                                                        </Link>
-                                                    </div>
-                                                </div>
-                                                <div className="inventory__column">
-                                                    <h4 className="inventory__item-header">CATEGORY</h4>
-                                                    <div className="inventory__category">{inventory.category}</div>
-                                                </div>
-                                            </div>
+                    </form>
+                )
+            }
 
-                                            <div className="inventory__section--right">
-                                                <div className="inventory__column">
-                                                    <h4 className="inventory__item-header">STATUS</h4>
-                                                    <div className={inventory.status === "In Stock" ? "inventory__status inventory__in-stock" : "inventory__status inventory__out-of-stock"}>
-                                                        {inventory.status.toUpperCase()}
-                                                    </div>
-                                                </div>
-                                                <div className="inventory__column">
-                                                    <h4 className="inventory__item-header">QTY</h4>
-                                                    <div className="inventory__qty">{inventory.quantity}</div>
-                                                </div>
-                                                <div className="inventory__column">
-                                                    <h4 className="inventory__item-header">WAREHOUSE</h4>
-                                                    <div className="inventory__warehouse">{inventory.warehouse_name}</div>
-                                                </div>
+            <ul className="inventory__list">
+                <li key="0" className="inventory__tablet-li">
+                    <div className="inventory__tablet-sectionwraper">
+                        <div className="inventory__tablet-section--left">
+                            <div className="inventory__tablet-column">
+                                <h4 className="inventory__tablet-header">INVENTORY ITEM</h4>
+                                <img className="inventory__tablet-icon" alt="sort icon" src={sortIcon} />
+                            </div>
+                            <div className="inventory__tablet-column">
+                                <h4 className="inventory__tablet-header">CATEGORY</h4>
+                                <img className="inventory__tablet-icon" alt="sort icon" src={sortIcon} />
+                            </div>
+                        </div>
+                        <div className={displayWarehouse ? "inventory__tablet-section--right" : "inventory__tablet-section--right2"}>
+                            <div className="inventory__tablet-column">
+                                <h4 className="inventory__tablet-header inventory__tablet-header--status">STATUS</h4>
+                                <img className="inventory__tablet-icon" alt="sort icon" src={sortIcon} />
+                            </div>
+                            <div className="inventory__tablet-column">
+                                <h4 className="inventory__tablet-header inventory__tablet-header--qty">QTY</h4>
+                                <img className="inventory__tablet-icon" alt="sort icon" src={sortIcon} />
+                            </div>
+                            {displayWarehouse && (<div className="inventory__tablet-column">
+                                <h4 className="inventory__tablet-header">WAREHOUSE</h4>
+                                <img className="inventory__tablet-icon" alt="sort icon" src={sortIcon} />
+                            </div>)}
+                        </div>
+                    </div>
+                    <div className="inventory__tablet-action">
+                        <h4 className="inventory__tablet-header">ACTIONS</h4>
+                    </div>
+                </li>
+
+                {
+                    inventoryList &&
+                    inventoryList
+                        .map((inventory, index) => (
+                            <>
+                                {index !== 0 && <hr className="inventory__line" />}
+                                <li key={inventory.id} className="inventory__li">
+                                    <div className="inventory__sectionwraper">
+                                        <div className="inventory__section--left">
+                                            <div className="inventory__column">
+                                                <h4 className="inventory__item-header">INVENTORY ITEM</h4>
+                                                <Link className="inventory__item-link" to={`/inventory/${inventory.id}`}>
+                                                    <div className="inventory__item--blue">{inventory.item_name}</div>
+                                                    <img className="inventory__item-icon" alt="right icon" src={rightIcon} />
+                                                </Link>
+                                            </div>
+                                            <div className="inventory__column">
+                                                <h4 className="inventory__item-header">CATEGORY</h4>
+                                                <div className="inventory__category">{inventory.category}</div>
                                             </div>
                                         </div>
 
-                                        <div className="inventory__actions">
-                                            <img src={deleteIcon} alt="delete icon" onClick={handleDelete} />
-                                            <img src={editIcon} alt="edit icon" onClick={handleEdit} />
+                                        <div className={displayWarehouse ? "inventory__section--right" : "inventory__section--right2"}>
+                                            <div className="inventory__column">
+                                                <h4 className="inventory__item-header">STATUS</h4>
+                                                <div className={inventory.status === "In Stock" ? "inventory__status inventory__in-stock" : "inventory__status inventory__out-of-stock"}>
+                                                    {inventory.status.toUpperCase()}
+                                                </div>
+                                            </div>
+                                            <div className="inventory__column">
+                                                <h4 className="inventory__item-header">QTY</h4>
+                                                <div className="inventory__qty">{inventory.quantity}</div>
+                                            </div>
+                                            {
+                                                displayWarehouse && (
+                                                    <div className="inventory__column">
+                                                        <h4 className="inventory__item-header">WAREHOUSE</h4>
+                                                        <div className="inventory__warehouse">{inventory.warehouse_name}</div>
+                                                    </div>
+                                                )
+                                            }
                                         </div>
-                                    </li >
-                                </>
-                            ))
-                    }
-                </ul>
-            </div>
+                                    </div>
+
+                                    <div className="inventory__actions">
+                                        <img src={deleteIcon} alt="delete icon" onClick={handleDelete} />
+                                        <img src={editIcon} alt="edit icon" onClick={handleEdit} />
+                                    </div>
+                                </li >
+                            </>
+                        ))
+                }
+            </ul>
 
         </div >
     );
